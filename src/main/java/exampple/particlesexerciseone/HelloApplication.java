e2189c8cda41249425abc3cd0f6d41c029ecb52e
nikolamurgo <murgovskinikola@gmail.com>
2023-12-27 15:06:12 +0100

    test for 10 particles

--- src/main/java/exampple/particlesexerciseone/HelloApplication.java
+++ src/main/java/exampple/particlesexerciseone/HelloApplication.java
@@ -1,7 +1,9 @@
 package exampple.particlesexerciseone;
 
 import javafx.animation.AnimationTimer;
+import javafx.animation.PathTransition;
 import javafx.application.Application;
+import javafx.geometry.Point2D;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -10,28 +12,36 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class HelloApplication extends Application {
+
+    ArrayList<Particle> particles = new ArrayList<Particle>();
+
     @Override
     public void start(Stage stage) throws IOException {
-        stage.setTitle("Particle Simulation");
+        stage.setTitle("Particle Effect"); // gui title
 
         StackPane root = new StackPane();
-        Canvas canvas = new Canvas(800, 600);
+        Canvas canvas = new Canvas(800, 600); // canvas size
         root.getChildren().add(canvas);
 
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        Particle particle = new Particle(new javafx.geometry.Point2D(200, 200));
+//        Particle particle = new Particle(new Point2D(400, 100));
+        for (int i=0; i<10; i++){
+            particles.add(new Particle());
+        }
         Scene scene = new Scene(root,800,600,Color.WHITE);
-
 
         new AnimationTimer() {
             @Override
             public void handle(long now) {
                 gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-                particle.update();
-                particle.display(gc);
+                for (Particle p : particles){
+                    p.update();
+                    p.display(gc);
+                }
 
             }
         }.start();
--- src/main/java/exampple/particlesexerciseone/Particle.java
+++ src/main/java/exampple/particlesexerciseone/Particle.java
@@ -5,28 +5,38 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public class Particle {
-    private Point2D location; // x and y coordinates
-    private Point2D velocity;
-    private Point2D acceleration;
-    private double lifespan=1.0;
+    public Point2D location; // x and y coordinates represented as a vector
+    private Point2D velocity; // direction vector
+    private Point2D acceleration; // speeding
+    private double lifespan=1.0; // for transparency and killing particle
 
-    public Particle(Point2D l) {
-        location = new Point2D(l.getX(), l.getY());
-        acceleration = new Point2D(0,0.01);
-        velocity = new Point2D(0,0);
+    // Constructor
+    public Particle() {
+        location = new Point2D(400,100);
+        acceleration = new Point2D(0,0.07);
+        velocity = new Point2D(Math.random(),Math.random());
     }
 
     public void update(){
         velocity = velocity.add(acceleration);
         location = location.add(velocity);
-        lifespan -=0.005;
+        lifespan -=0.01;
         if (lifespan <= 0){
             lifespan =0;
         }
     }
 
+    // method t/f for life of particle
+    boolean killParticle(){
+        if(lifespan <= 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
     public void display(GraphicsContext gc) {
-        gc.setFill(Color.color(1.0,0.0,0.0, lifespan));
-        gc.fillOval(location.getX(),location.getY(),40,40);
+        gc.setFill(Color.color(1.0,0.0,0.0, lifespan)); // set color of particle and alpha
+        gc.fillOval(location.getX(),location.getY(),10,10); // size of particle
     }
 }
